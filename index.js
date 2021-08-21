d3477765d59b471921d457bfe4e76f643da2bb48
Mayur Luhar <83022097+mayur778@users.noreply.github.com>
2021-08-21 22:53:30 +0530

    small port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 var requests = require("requests");
-const port = process.env.PORT || 8000;
+const port = process.env.port || 8000;
 
 
 const homeFile = fs.readFileSync("home.html", "utf-8");
